aa95f630ca401107150546aefc0fb15af2b02f88
LHK-7 <lk2811@columbia.edu>
2020-07-31 16:54:43 -0400

    fix error

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,9 @@
 if (process.env.NODE_ENV === 'prodcution'){
   //production version
   module.exports = require('./prod');
-} else {
-  //development version
-  module.exports = require('./dev');
 }
+// else {
+//   //development version
+//   module.exports = require('./dev');
+// }
 // prod.DB :mongodb+srv://adminUser:Hanh5336!@cluster0.1nuci.mongodb.net/<dbname>?retryWrites=true&w=majority
