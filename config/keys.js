8ca5afd3db5f0c169707b162ce57a4ab1961c53f
LHK-7 <lk2811@columbia.edu>
2020-07-31 17:34:25 -0400

    hardcode

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,7 @@
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'prodcution'){
   //production version
-  googleClientID =  process.env.GOOGLE_CLIENT_ID;
-  googleClientSecret =  process.env.GOOGLE_CLIENT_SECRET;
-  mongoURI = process.env.MONGO_URI;
-  cookieKey = process.env.COOKIEKEY
-  // module.exports = require('./prod');
+  module.exports = require('./prod');
 }
 // else {
 //   //development version
--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // prodcution version
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIEKEY
+  googleClientID: "376212848089-hh4kk9lnr2hkhlr582v5cs4bheg8ur59.apps.googleusercontent.com",
+  googleClientSecret: "RlR-gnCHIK4xi2vRq_KpNnDC",
+  mongoURI: "mongodb+srv://adminUser:Hanh5336!@cluster0.1nuci.mongodb.net/LHK_prod?retryWrites=true&w=majority",
+  cookieKey: "asfdhviudhvuhoijdqwndiwhdiquhwu"
 }
